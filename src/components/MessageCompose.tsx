5cd2bf7a17e27153c35785869d45d5c7abfb4b76
Elmar Burke <hi@elmar.codes>
2020-10-06 16:25:38 +0200

    controlled component

--- src/components/MessageCompose.tsx
+++ src/components/MessageCompose.tsx
@@ -3,15 +3,23 @@ import React from "react";
 interface Props {}
 
 const MessageCompose: React.FunctionComponent<Props> = () => {
+  const [inputValue, setInput] = React.useState("");
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     console.log("message");
   };
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
+      <input onChange={handleChange} value={inputValue} />
       <button type="submit">Absenden</button>
+      <p>aktuell in state: {inputValue}</p>
     </form>
   );
 };
