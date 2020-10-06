47bad86f4800175644c750635bdaa0246f8abdac
Elmar Burke <hi@elmar.codes>
2020-10-06 16:50:35 +0200

    lifting state up!

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import MessageCompose from "./components/MessageCompose";
 import MessageList from "./components/MessageList";
+import { Message } from "./domain";
+
+const messages: Message[] = [
+  { id: "a", author: "Christian", message: "Das bin ich (nicht)", date: 2 },
+  { id: "b", author: "Elmar", message: "Das bin ich (nicht)", date: 3 },
+  { id: "c", author: "Christian", message: "Wohl!", date: 4 },
+];
 
 function App() {
+  const [state, setState] = React.useState(messages);
+
   const handleMessageSend = (text: string) => {
     console.log("new message", text);
+
+    const newMessage = {
+      author: "elmar",
+      message: text,
+      id: `${Date.now()}`,
+      date: Date.now(),
+    };
+
+    setState((oldState) => [...oldState, newMessage]);
   };
 
   return (
     <>
-      <MessageList />
+      <MessageList messages={state} />
 
       <MessageCompose onMessageSend={handleMessageSend} />
     </>
--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import { Message } from "../domain";
 import MessageView from "./MessageView";
 
-const messages: Message[] = [
-  { id: "a", author: "Christian", message: "Das bin ich (nicht)", date: 2 },
-  { id: "b", author: "Elmar", message: "Das bin ich (nicht)", date: 3 },
-  { id: "c", author: "Christian", message: "Wohl!", date: 4 },
-];
+interface Props {
+  messages: Message[];
+}
 
-interface Props {}
-
-const MessageList: React.FunctionComponent<Props> = () => {
+const MessageList: React.FunctionComponent<Props> = ({ messages }) => {
   return (
     <div role="list">
       {messages.map((message) => (
