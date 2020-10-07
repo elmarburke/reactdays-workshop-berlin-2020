1b54f804f2eb34ab7f20894155a9a7101a90145f
Elmar Burke <hi@elmar.codes>
2020-10-07 14:27:30 +0200

    load messages in redux

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -4,10 +4,16 @@ import { Message } from "./domain";
 /* Actions */
 
 const ADD_MESSAGE = "ADD_MESSAGE";
+const LOAD_MESSAGE_SUCCEEDED = "LOAD_MESSAGE_SUCCEEDED";
 
 export const addMessage = (message: Message) => ({
   type: ADD_MESSAGE,
   payload: { message },
+});
+
+export const loadMessageSucceeded = (messages: readonly Message[]) => ({
+  type: LOAD_MESSAGE_SUCCEEDED,
+  payload: { messages },
 });
 
 /* Reducer */
@@ -25,6 +31,13 @@ const reducer = (state = initialState, action: any): State => {
     return {
       ...state,
       messages: [...state.messages, action.payload.message],
+    };
+  }
+
+  if (action.type === LOAD_MESSAGE_SUCCEEDED) {
+    return {
+      ...state,
+      messages: action.payload.messages,
     };
   }
 
--- src/hooks/useMessages.ts
+++ src/hooks/useMessages.ts
@@ -1,6 +1,7 @@
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Message } from "../domain";
-import { State, addMessage } from "../configureStore";
+import { State, addMessage, loadMessageSucceeded } from "../configureStore";
 
 const useMessages = () => {
   const messages = useSelector((state: State) => state.messages);
@@ -8,11 +9,31 @@ const useMessages = () => {
 
   // const [, setState] = React.useState<readonly Message[]>([]);
 
-  // React.useEffect(() => {
-  //   fetch("/messages.json")
-  //     .then((response) => response.json())
-  //     .then((data) => setState(data));
-  // }, []);
+  React.useEffect(() => {
+    fetch("/messages.json")
+      .then((response) => response.json())
+      .then((data: unknown) => {
+        // in einer echten anwendung gucken wir ob die daten so sind wie es uns versprochen wurde.
+        // => TS ist nur zur Development time
+
+        if (!Array.isArray(data)) {
+          throw new TypeError("Data is not an array");
+        }
+
+        if (
+          !data.every((item) => {
+            return (
+              typeof item.id === "string" && typeof item.message === "string"
+            );
+          })
+        ) {
+          throw new TypeError("Data items are not valid");
+        }
+
+        //data.forEach((message) => dispatch(addMessage(message)));
+        dispatch(loadMessageSucceeded(data));
+      });
+  }, [dispatch]);
 
   const sendMessage = (message: Message): void => {
     dispatch(addMessage(message));
