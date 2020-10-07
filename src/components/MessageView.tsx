7da0693dd0e6b198326e518e8abe07025a32f05c
Elmar Burke <hi@elmar.codes>
2020-10-07 15:11:48 +0200

    styled components mit macro

--- src/components/MessageView.tsx
+++ src/components/MessageView.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // $ yarn add @types/styled-components
-import styled from "styled-components";
+import styled from "styled-components/macro";
 import { Message } from "../domain";
 
 const MessageItem = styled.article`
