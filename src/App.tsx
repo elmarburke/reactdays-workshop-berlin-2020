72bff83a3e1a9fb71b6ea497eaaa28b1fed0dcd2
Elmar Burke <hi@elmar.codes>
2020-10-07 13:47:21 +0200

    provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,6 @@ import React from "react";
 import MessageCompose from "./components/MessageCompose";
 import MessageList from "./components/MessageList";
 import useMessages from "./hooks/useMessages";
-import configureStore from "./configureStore";
-
-const store = configureStore();
-
-// @ts-ignore
-window.store = store;
 
 function App() {
   const { messages, sendMessage } = useMessages();
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// $ yarn add @types/react-redux
+// $ npm install @types/react-redux
+import { Provider } from "react-redux";
 import App from "./App";
 
-const element = <App />;
+import configureStore, { addMessage } from "./configureStore";
+
+const store = configureStore();
+
+// @ts-ignore
+window.store = store;
+
+store.dispatch(
+  addMessage({
+    id: `${Date.now()}-1`,
+    author: "Christian",
+    message: "Hallo Welt",
+    date: Date.now(),
+  })
+);
+
+store.dispatch(
+  addMessage({
+    id: `${Date.now()}-2`,
+    author: "Elmar",
+    message: "Hallo Welt",
+    date: Date.now(),
+  })
+);
+
+const element = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 ReactDOM.render(element, document.getElementById("root"));
